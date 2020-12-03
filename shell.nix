250552770b993daffd5067282afaf2ae9dc8bd20
Daniel Wheeler <daniel.wheeler2@gmail.com>
2020-12-03 14:54:00 -0500

    setup for Binder

---
+++ shell.nix
@@ -0,0 +1,32 @@
+{ pkgs ? (import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
+    sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
+  }) {}) }:
+let
+  pypkgs = pkgs.python3Packages;
+  cmocean = pypkgs.buildPythonPackage rec {
+    pname = "cmocean";
+    version = "2.0";
+    src = pypkgs.fetchPypi {
+      inherit pname version;
+      sha256 = "0fmhm7p1farwcq77n7mj3jsdyvkgd0z0pnx268z313jdk74a7vhk";
+    };
+    buildInputs = with pypkgs; [
+      pytest
+      numpy
+      matplotlib
+    ];
+  };
+in
+  pkgs.mkShell rec {
+    pname = "graspi-env";
+    nativeBuildInputs = with pypkgs; [
+      xarray
+      netcdf4
+      cartopy
+      jupyter
+      scipy
+      pip
+      cmocean
+    ];
+  }
